2acf1fa98e0a6f2effe48cd520646bebcad065cf
aliciapflaumer <alpfla7@gmail.com>
2017-11-09 13:33:59 -0500

    Received successful `read & `delete` curl scripts

--- scripts/plans/update.sh
+++ scripts/plans/update.sh
@@ -5,13 +5,13 @@ URL_PATH="/plans/${ID}"
 curl "${API}${URL_PATH}" \
   --include \
   --request PATCH \
-  --header "Authorization: Token token=${TOKEN}"
+  --header "Authorization: Token token=BAhJIiU0NDVjM2E0MGM2OWNiZWI1MDM4N2M2NWI5NjBjMmEzNQY6BkVG--a784aad768bac90566f4aca1b85ae366b64dd687"
   --data '{
     "plan": {
-      "name": "'"${NAME}"'",
-      "date": "'"${DATE}"'",
-      "location": "'"${LOCATION}"'",
-      "difficulty": "'"${DIFFICULTY}"'"
+      "name": "Cardio",
+      "date": "2017-11-10",
+      "location": "indoors",
+      "difficulty": "moderate"
     }
   }'
 
